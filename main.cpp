05afedf7dd6804a859201cd34286b28af7dcc2ed
jangelmm <jesusangelmartinezmendoza0702@gmail.com>
2025-03-15 16:34:50 -0600

    Agregar mensajes de inicio y eliminar archivo programa.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,17 @@
 #include <chrono>
 #include <iostream>
 
+using namespace std;
+
 int main() {
     Juego juego;
+
     while (!juego.terminado()) {
         juego.dibujar();
         juego.actualizar();
         std::this_thread::sleep_for(std::chrono::milliseconds(200));
     }
 
-    std::cout << "\nJuego terminado!" << std::endl;
+    cout<< "\nJuego terminado!" <<endl;
     return 0;
 }
